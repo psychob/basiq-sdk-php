e2f129f54bd43db05a64a79237908e3d9e45f0e5
Andrzej Budzanowski <kontakt@andrzej.budzanowski.pl>
2020-06-03 14:17:36 +0200

    cast Account inside connection

--- src/entities/Connection.php
+++ src/entities/Connection.php
@@ -6,12 +6,15 @@ use Basiq\Services\ConnectionService;
 
 class Connection extends Entity {
 
+    /** @var ConnectionService */
     private $service;
     private $user;
 
     public $status;
     public $lastUsed;
     public $institution;
+
+    /** @var Account[] */
     public $accounts;
     
     public function __construct(ConnectionService $service, $user, $data)
@@ -20,25 +23,26 @@ class Connection extends Entity {
         $this->status = isset($data["status"]) ? (string) $data["status"] : null;
         $this->lastUsed = isset($data["lastUsed"]) ? new \DateTime($data["lastUsed"]) : null;
         $this->institution = isset($data["institution"]) ? $data["institution"] : null;
-        $this->accounts = isset($data["accounts"]) ? (array)$data["accounts"] : [];
+        $this->accounts = isset($data["accounts"]) ? array_map(function ($value) {
+            return new Account($value);
+        }, (array)$data["accounts"]) : [];
 
         $this->user = $user;
-
-        $this->connectionService = $service;
+        $this->service = $service;
     }
 
     public function update($data)
     {
-        return $this->connectionService->update($this->id, $data);
+        return $this->service->update($this->id, $data);
     }
 
     public function refresh()
     {
-        return $this->connectionService->refresh($this->id);
+        return $this->service->refresh($this->id);
     }
 
     public function delete()
     {
-        return $this->connectionService->delete($this->id);
+        return $this->service->delete($this->id);
     }
 }
